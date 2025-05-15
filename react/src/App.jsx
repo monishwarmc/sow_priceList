d6957e3f74fab4c2d809081555a525e828f1f401
monishwarmc <monishwar69@gmail.com>
2025-05-15 20:52:42 +0530

    good

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <div className='text-balck text-6xl'
-        >Price list</div>
-    </>
+    <div className='flex flex-col h-screen'>
+      <Header />
+      <div className='flex flex-row justify-start h-full items-center bg-gray-100 text-black'>
+        <Navbar />
+        <h1 className=''>Hello World</h1>
+      </div>
+    </div>
   )
 }
 
---
+++ react/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+
+    const menuNames = ["Menu","Invoices","Customer register","My Business","Invoice Journal","Price List","Multiple Invoicing","Unpaid Invoices","Offer","Inventory Control","Member Invoicing","Import and Export","Support","Log Out"];
+    const menuIcons = [ "/Menu.png","/Invoices.png","/Customer register.png","/My Business.png","/Invoice Journal.png","/Price List.png","/Multiple Invoicing.png","/Unpaid Invoices.png","/Offer.png","/Inventory Control.png","/Member Invoicing.png","/Import and Export.png","/Support.png","/Log Out.png"];
+
+  return (
+    <div className='z-10 pt-2 w-60 h-full border-r-8 flex flex-col items-center'>
+        <h1 className='text-2xl font-bold mb-2'>Menu</h1>
+        <hr className='border-2 border-blue-600 mb-4 rounded w-48 ' />
+        <div className='flex flex-col items-center'>
+            {menuNames.slice(1).map((name, index) => (
+                <div key={index+1} className='rounded-2xl flex flex-row items-center justify-start w-full h-12 px-4 hover:bg-blue-100 cursor-pointer'>
+                    <img src={menuIcons[index+1]} alt={name} className='w-6 h-6 mr-2' />
+                    <p className='text-lg'>{name}</p>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
